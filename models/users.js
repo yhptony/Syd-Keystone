8881c5ee8b6ce92c8ca643eeb295c00689b99722
Tonny Yan <yhppenny@gmail.com>
2015-05-25 11:32:20 +1000

    add user model

---
+++ models/users.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var keystone = require('keystone'),
+    Types = keystone.Field.Types;
+
+//schema
+var User = new keystone.List('User');
+
+User.add({
+    name: { type: Types.Name, required: true, index: true },
+    email: { type: Types.Email, initial: true, required: true, index: true },
+    password: { type: Types.Password, initial: true },
+    canAccessKeystone: { type: Boolean, initial: true }
+});
+
+User.register();
